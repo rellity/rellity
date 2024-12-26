945e1d0d25d0c1e4b0700bc53fa064e6b258a85c
rellityuint32 <zachari.iligan@asceoft.com>
2024-12-27 03:10:07 +0800

    update image paths

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -20,19 +20,19 @@ export const personalProjects = [
     {
         name: 'ICTS Logger',
         stack: 'React-Native, Expo, Javascript, PHP, MySql',
-        logo: 'https://media.discordapp.net/attachments/915116526804291674/1313116420934209606/CCSIT.png?ex=674ef677&is=674da4f7&hm=4b92a1323e6bad15606c8dc5a01dc5447531288acb1d268f8667150063374602&=&format=webp&quality=lossless&width=352&height=350',
+        logo: '/projects/CCSIT.png',
         url: 'https://github.com/rellity/appattthree',
     },
     {
         name: "Tracker Clone - Webflow",
         stack: 'React, NextJS, Tailwind, TypeScript',
-        logo: 'https://media.discordapp.net/attachments/915116526804291674/1313115803368816651/tracker.png?ex=674ef5e4&is=674da464&hm=de46bbf5a84cb2190c6bb49ecbef04194d11533a92fc2de19f44673daaf061f0&=&format=webp&quality=lossless&width=911&height=543',
+        logo: '/projects/tracker.png',
         url: 'https://github.com/rellity/tracker-clone',
     },
     {
         name: 'Pocket Clone - Webflow',
         stack: 'React, NextJS, Tailwind, TypeScript',
-        logo: 'https://media.discordapp.net/attachments/915116526804291674/1313115803754823750/pocket.png?ex=674ef5e4&is=674da464&hm=1ab21a7cefead1a7c40e63d2f52521f761af1ea65f5e8a4474e3fe2f60d64760&=&format=webp&quality=lossless&width=815&height=587',
+        logo: '/projects/pocket.png',
         url: 'https://github.com/rellity/pocket-clone',
     },
 ]
